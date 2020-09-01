84676356d5e5bcc2dd658ab16a29d949f6a9f227
pete-strongman <peteman@bluewin.ch>
2020-09-01 23:31:29 +0200

    Create an Item

--- js/kanban.js
+++ js/kanban.js
@@ -16,6 +16,7 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  var item = document.createElement('div');
 };
 
 document.querySelectorAll('.drop').forEach(element => {
